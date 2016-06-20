ce6de890d36e08c249b3df4e2a92360132a089fb
rbuda <ryannebuda@gmail.com>
2016-06-20 17:16:24 -0500

    finalized solution using recursion

--- blankcheck.rb
+++ blankcheck.rb
@@ -1,49 +1,57 @@
 # create a function to take in a number and produce text as if a user needed to see what to write in a check.
 
 TRANSLATE = {
-  "0" => "zero",
-  "1" => "one",
-  "2" => "two",
-  "3" => "three",
-  "4" => "four",
-  "5" => "five",
-  "6" => "six",
-  "7" => "seven",
-  "8" => "eight",
-  "9" => "nine",
-  "10" => "ten",
-  "11" => "eleven",
-  "12" => "twelve",
-  "13" => "thirteen",
-  "14" => "fourteen",
-  "15" => "fifteen",
-  "16" => "sixteen",
-  "17" => "seventeen",
-  "18" => "eighteen",
-  "19" => "nineteen",
-  "20" => "twenty",
-  "30" => "thirty",
-  "40" => "forty",
-  "50" => "fifty",
-  "60" => "sixty",
-  "70" => "seventy",
-  "80" => "eighty",
-  "90" => "ninety"
+  1000000 => "million",
+  1000 => "thousand",
+  100 => "hundred",
+  90 => "ninety",
+  80 => "eighty",
+  70 => "seventy",
+  60 => "sixty",
+  50 => "fifty",
+  40 => "forty",
+  30 => "thirty",
+  20 => "twenty",
+  19 => "nineteen",
+  18 => "eighteen",
+  17 => "seventeen",
+  16 => "sixteen",
+  15 => "fifteen",
+  14 => "fourteen",
+  13 => "thirteen",
+  12 => "twelve",
+  11 => "eleven",
+  10 => "ten",
+  9 => "nine",
+  8 => "eight",
+  7 => "seven",
+  6 => "six",
+  5 => "five",
+  4 => "four",
+  3 => "three",
+  2 => "two",
+  1 => "one"
 }
-num = ARGV[0]
 
-def string(num)
-  length = num.split("").length
-  case length
-  when (0..2)
-    if TRANSLATE[num].is_a? String
-      puts TRANSLATE[num]
-    else arr = num.to_i.divmod(10)
-      puts TRANSLATE[arr[0].to_s + "0"] + "-" + TRANSLATE[arr[1].to_s]
+def in_words(int)
+  str = ""
+  TRANSLATE.each do |num, name|
+    if int == 0
+      return str + "zero"
+    elsif int == num
+      return str + "#{name}"
+    elsif int < 100 && int/num > 0
+      return str + "#{name}" if int%num == 0
+      return str + "#{name} " + in_words(int%num)
+    elsif int/num > 0
+      # puts "recursion"
+      return str + in_words(int/num) + " #{name} " + in_words(int%num)
     end
-  when (2..9)
-    puts "nein"
   end
 end
 
-string(num)
+# puts in_words(4)
+# puts in_words(44)
+# puts in_words(440)
+puts in_words(4040)
+puts in_words(4004001)
